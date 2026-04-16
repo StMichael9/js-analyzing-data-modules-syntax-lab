c99abf177bb04563f962400316051d6412645584
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-16 15:15:47 -0700

    Finished lab. Will run tests to make sure

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+require("datejs");
 
+const combineUsers = (...args) => {
+  const combinedObject = {
+    users: [],
+    merge_date: "",
+  };
 
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+  return combinedObject;
+};
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
